e36196e434be643873515373e13e6ee94255824a
Artem <dr.artem-k@yandex.ru>
2021-02-28 10:47:12 +0400

    edit readme

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./images/logo-white.png";
+import loupe from "./images/loupe.png";
+import user from "./images/user.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <img src={logo} className="logo" alt="логотип" />
+        <form className="search">
+          <input className="search__input" placeholder="Введите тему курса" required />
+          <button className="search__loupe"><img src={loupe} alt="лупа" /></button>
+        </form>
+        <ul className="header__user-menu">
+          <li className="header__item header__item_name">
+            <button className="button header__button">Юлия<img src={user} /></button>
+            <ul className="header__button-menu">
+              <li className="header__item" id="lkButton">
+                <a className="category__link" href="">Личный кабинет</a>
+              </li>
+              <li className="header__item link" id="mainButton">
+                <a className="category__link" href="">Главная</a>
+              </li>
+              <li className="header__item link" id="favoritesButton">
+                <a className="category__link" href="">Избранное</a>
+              </li>
+              <li className="header__item link">
+                <a className="category__link" href="">Создать курс</a>
+              </li>
+            </ul>
+          </li>
+          <li className="header__item header__item_auth  hidden" id="login-button-header">
+            <button className="button header__button">Авторизоваться</button>
+          </li>
+        </ul>
       </header>
     </div>
   );
